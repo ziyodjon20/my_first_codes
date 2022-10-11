f15b2f8c4df723ed665ddac7de7df6d2f4046f7f
ziyodjon20 <84076406+ziyodjon20@users.noreply.github.com>
2022-10-11 15:35:00 +0500

    Add files via upload

---
+++ ro'yxat.py
@@ -0,0 +1,7 @@
+a = input("O'quvchining ismini kiriting: ")
+b = input("O'quvchining ismini kiriting: ")
+c = input("O'quvchining ismini kiriting: ")
+d = input("O'quvchining ismini kiriting: ")
+e = input("O'quvchining ismini kiriting: ")
+list = [a, b, c, d, e, "Farhod", "Jamshid", "Shag'zod", "Alisher" ]
+print(list)
