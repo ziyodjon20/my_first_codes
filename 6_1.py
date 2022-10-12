e5fa5e1f318e6607b08d0e29a0c79206737e51fb
ziyodjon20 <84076406+ziyodjon20@users.noreply.github.com>
2022-10-12 08:32:14 +0500

    Add files via upload

---
+++ 6_1.py
@@ -0,0 +1,9 @@
+#6-dars 1-vazifa
+a = int(input('Sonni kiriting: '))
+
+if a > 100:
+    print('Kiritilgan son 100 dan katta')
+elif a < 100:
+    print('Kiritilgan son 100 dan kichik')
+else:
+    print('Kiritilgan son 100 ga teng')
